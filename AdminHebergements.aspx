96a926aea2db82cce786e717b5b5024b928bb9e3
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-01-22 13:15:09 +0100

    ras

---
+++ AdminHebergements.aspx
@@ -0,0 +1,4 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Administration.master" AutoEventWireup="true" CodeBehind="AdminHebergements.aspx.cs" Inherits="Admin_MRLoca.AdminHebergements" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentMain2" runat="server">
+
+</asp:Content>
--- AdminHebergements.aspx.cs
+++ AdminHebergements.aspx.cs
@@ -7,15 +7,11 @@ using System.Web.UI.WebControls;
 
 namespace Admin_MRLoca
 {
-    public partial class AdminHome : System.Web.UI.Page
+    public partial class AdminHebergements : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            Admin admin = (Admin)Session["Admin"];
-            if(admin != null)
-            {
-                this.lblAdmin.Text = admin.Login + ' ' + admin.Password; 
-            }
+
         }
     }
 }
