7fe725ea8c13a88b3db3f39437ff58ce793d53df
Maxime Rogogine <rogogine.maxime@gmail.com>
2020-01-23 11:38:24 +0100

    ajout de Dao

---
+++ Dao/DataAccess.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Admin_MRLoca.Dao
+{
+    public class DataAccess
+    {
+        public MRLocaEntities mRLocaEntities { get; set; }
+        public DataAccess()
+        {
+            mRLocaEntities = new MRLocaEntities();
+        }
+    }
+}
